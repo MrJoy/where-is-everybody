0613693cf8a1cfb766723189d0499b279f51cb92
Jon Frisby <jfrisby@mrjoy.com>
2013-04-24 02:36:48 -0700

    Adding our first, simple test case.  Yay!

---
+++ simulator/tests/hammer_random.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+if [[ $ATTEMPTS -eq 0 ]]; then
+  export ATTEMPTS=1000
+fi
+
+export FAIL=0
+echo "TEST: Hammering 'bin/generate_randoms --first --last --seed=1' $ATTEMPTS times to try and reproduce device-allocation error."
+for i in {1..1000}; do
+  RESULT=$(bin/generate_randoms --first --last --seed=1 2>&1 |
+    fgrep -v 'Block #' |
+    fgrep -v 'Generating 1 block(s) of 67108864 samples.' |
+    fgrep -v 'Using seed: 1' |
+    grep -E '^ERROR:')
+
+  if [[ $RESULT != '' ]]; then
+    echo $RESULT
+    echo "FAIL: Got an unexpected error for a case that should pretty much always work."
+    exit 1
+  fi
+done
+
+echo "PASS: Got no errors in generate_randoms after $ATTEMPTS attempts."
