cc903c3642b5d9b329f703afe2f67bf9a30597a1
Jon Frisby <jfrisby@mrjoy.com>
2013-04-24 04:08:31 -0700

    Simplify this a lot.

--- simulator/tests/hammer_random.sh
+++ simulator/tests/hammer_random.sh
@@ -6,17 +6,13 @@ elif [[ $ATTEMPTS -eq 0 ]]; then
 fi
 
 export FAIL=0
-echo "TEST: Hammering 'bin/randoms --first --last --samples=67108864 --blocks=1 --seed=1' $ATTEMPTS times to try and reproduce device-allocation error."
+echo "TEST: Hammering 'bin/randoms --quiet --samples=67108864 --blocks=1 --seed=1' $ATTEMPTS times to try and reproduce device-allocation error."
 for i in $(seq 1 $ATTEMPTS); do
   if [[ ! -x bin/randoms ]]; then
     echo "FAIL: Need to run 'make all' first."
     exit 1
   fi
-  RESULT=$(bin/randoms --first --last --samples=67108864 --blocks=1 --seed=1 2>&1 |
-    fgrep -v 'Block #' |
-    fgrep -v 'Generating 1 block(s) of 67108864 samples.' |
-    fgrep -v 'Using seed: 1' |
-    grep -E '^ERROR:')
+  RESULT=$(bin/randoms --quiet --samples=67108864 --blocks=1 --seed=1 2>&1)
 
   if [[ $RESULT != '' ]]; then
     echo $RESULT
