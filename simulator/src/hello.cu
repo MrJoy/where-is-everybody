2fa749fcbd0a163bf4355ce8403d51e9997d5dc9
Jon Frisby <jfrisby@mrjoy.com>
2013-04-25 02:33:26 -0700

    Improve output formatting a smidge.

--- simulator/src/hello.cu
+++ simulator/src/hello.cu
@@ -8,10 +8,10 @@ __global__
 void
 hello(unsigned int *seeds, unsigned int *outs, int n)
 {
-  curandStateXORWOW_t rgen ;
-  curand_init(seeds[threadIdx.x], 0,0,&rgen);
-  for(int i=0; i<n; ++i){
-    outs[threadIdx.x * n + i] = curand(&rgen) ;
+  curandStateXORWOW_t rgen;
+  curand_init( seeds[threadIdx.x], 0, 0, &rgen );
+  for( int i=0; i<n; ++i ) {
+    outs[threadIdx.x * n + i] = curand( &rgen );
   }
   //see float curand_uniform
   //see curand__discrete
@@ -22,19 +22,21 @@ get_seeds( unsigned int *seeds, int n )
 {
   FILE* randomSource = fopen("/dev/random", "rb");
   unsigned int seed0;
-  int recordsRead = fread(&seed0, sizeof(unsigned long), 1, randomSource);
+  int recordsRead = fread( &seed0, sizeof(unsigned long), 1, randomSource );
   //assert(recordsRead == n);
-  for(int i=0; i<n; ++i){
+  for( int i=0; i<n; ++i ) {
     seeds[i] = seed0 + i;
   }
-  fclose(randomSource);
+  fclose( randomSource );
 }
 
-void inspect(unsigned int *array, int n){
-  for(int i=0; i< n; ++i){
-    printf("%x ", array[i]);
+void
+inspect( unsigned int *array, int n )
+{
+  for( int i=0; i<n; ++i ) {
+    printf( "%08x ", array[i] );
   }
-  printf("\n");
+  printf( "\n" );
 }
 
 int
@@ -45,11 +47,11 @@ main()
 
   unsigned int *cseeds;
   unsigned int *coutputs;
-  const int seed_size = N*sizeof(unsigned int);
-  const int output_size = N*N*sizeof(unsigned int);
+  const int seed_size = N*sizeof( unsigned int );
+  const int output_size = N*N*sizeof( unsigned int );
 
-  get_seeds(&seeds[0], N );
-  inspect(seeds, N);
+  get_seeds( &seeds[0], N );
+  inspect( seeds, N );
 
   cudaMalloc( (void**)&cseeds, seed_size );
   cudaMalloc( (void**)&coutputs, output_size );
@@ -59,13 +61,13 @@ main()
   dim3 dimBlock( blocksize, 1 );
   dim3 dimGrid( 1, 1 );
   //TODO, don't pass N, use dim
-  hello<<<dimGrid, dimBlock>>>(cseeds, coutputs, N);
+  hello<<<dimGrid, dimBlock>>>( cseeds, coutputs, N );
   cudaMemcpy( outputs, coutputs, output_size, cudaMemcpyDeviceToHost );
   cudaFree( cseeds );
   cudaFree( coutputs );
 
-  for(int i=0; i<N; ++i){
-    inspect(outputs + i * N, N );
+  for( int i=0; i<N; ++i ) {
+    inspect( outputs+i*N, N );
   }
   return EXIT_SUCCESS;
 }
