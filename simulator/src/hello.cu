d222da33fe1732aae12d4f61158cd4a61644c0fa
Jon Frisby <jfrisby@mrjoy.com>
2013-04-25 02:31:04 -0700

    Initial checkin of Mat's code.

--- simulator/src/hello.cu
+++ simulator/src/hello.cu
@@ -1,43 +1,67 @@
-// This is the REAL "hello world" for CUDA!
-// It takes the string "Hello ", prints it, then passes it to CUDA with an array
-// of offsets. Then the offsets are added in parallel to produce the string "World!"
-// By Ingemar Ragnemalm 2010
- 
 #include <stdio.h>
- 
-const int N = 16; 
-const int blocksize = 16; 
- 
-__global__ 
-void hello(char *a, int *b) 
+#include "curand_kernel.h"
+
+const int N = 16;
+const int blocksize = 16;
+
+__global__
+void hello(unsigned int *seeds, unsigned int *outs, int n)
 {
-  a[threadIdx.x] += b[threadIdx.x];
+  curandStateXORWOW_t rgen ;
+  curand_init(seeds[threadIdx.x], 0,0,&rgen);
+  for(int i=0; i<n; ++i){
+    outs[threadIdx.x * n + i] = curand(&rgen) ;
+  }
+  //see float curand_uniform
+  //see curand__discrete
 }
- 
+
+void get_seeds(unsigned int *seeds, int n){
+  FILE* randomSource = fopen("/dev/random", "rb");
+  unsigned int seed0;
+  int recordsRead = fread(&seed0, sizeof(unsigned long), 1, randomSource);
+  //assert(recordsRead == n);
+  for(int i=0; i<n; ++i){
+    seeds[i] = seed0 + i;
+  }
+  fclose(randomSource);
+}
+
+void inspect(unsigned int *array, int n){
+  for(int i=0; i< n; ++i){
+    printf("%x ", array[i]);
+  }
+  printf("\n");
+}
+
 int main()
 {
-  char a[N] = "Hello \0\0\0\0\0\0";
-  int b[N] = {15, 10, 6, 0, -11, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
- 
-  char *ad;
-  int *bd;
-  const int csize = N*sizeof(char);
-  const int isize = N*sizeof(int);
- 
-  printf("%s", a);
- 
-  cudaMalloc( (void**)&ad, csize ); 
-  cudaMalloc( (void**)&bd, isize ); 
-  cudaMemcpy( ad, a, csize, cudaMemcpyHostToDevice ); 
-  cudaMemcpy( bd, b, isize, cudaMemcpyHostToDevice ); 
-  
+  unsigned int seeds[N];
+  unsigned int outputs[N * N];
+
+  unsigned int *cseeds;
+  unsigned int *coutputs;
+  const int seed_size = N*sizeof(unsigned int);
+  const int output_size = N*N*sizeof(unsigned int);
+
+  get_seeds(&seeds[0], N );
+  inspect(seeds, N);
+
+  cudaMalloc( (void**)&cseeds, seed_size );
+  cudaMalloc( (void**)&coutputs, output_size );
+  cudaMemcpy( cseeds, seeds, seed_size, cudaMemcpyHostToDevice );
+  cudaMemcpy( coutputs, outputs, output_size, cudaMemcpyHostToDevice );
+
   dim3 dimBlock( blocksize, 1 );
   dim3 dimGrid( 1, 1 );
-  hello<<<dimGrid, dimBlock>>>(ad, bd);
-  cudaMemcpy( a, ad, csize, cudaMemcpyDeviceToHost ); 
-  cudaFree( ad );
-  cudaFree( bd );
-  
-  printf("%s\n", a);
+  //TODO, don't pass N, use dim
+  hello<<<dimGrid, dimBlock>>>(cseeds, coutputs, N);
+  cudaMemcpy( outputs, coutputs, output_size, cudaMemcpyDeviceToHost );
+  cudaFree( cseeds );
+  cudaFree( coutputs );
+
+  for(int i=0; i<N; ++i){
+    inspect(outputs + i * N, N );
+  }
   return EXIT_SUCCESS;
 }
