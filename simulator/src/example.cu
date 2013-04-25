88f53e28e65ab8f9815c463db5cff1bb1073cac0
Jon Frisby <jfrisby@mrjoy.com>
2013-04-25 16:14:00 -0700

    Simplify this.

--- simulator/src/example.cu
+++ simulator/src/example.cu
@@ -11,30 +11,25 @@
 #include <cuda.h>
 #include <curand_kernel.h>
 
+#define THREADS_PER_BLOCK 64
+#define BLOCKS 64
+
 #define CUDA_CALL(x) do { if((x) != cudaSuccess) { \
     printf("Error at %s:%d\n",__FILE__,__LINE__); \
     return EXIT_FAILURE;}} while(0)
 
 __global__ void setup_kernel(curandState *state)
 {
-    int id = threadIdx.x + blockIdx.x * 64;
+    int id = threadIdx.x + blockIdx.x * THREADS_PER_BLOCK;
     /* Each thread gets same seed, a different sequence
        number, no offset */
     curand_init(1234, id, 0, &state[id]);
 }
 
-__global__ void setup_kernel(curandStateMRG32k3a *state)
-{
-    int id = threadIdx.x + blockIdx.x * 64;
-    /* Each thread gets same seed, a different sequence
-       number, no offset */
-    curand_init(0, id, 0, &state[id]);
-}
-
 __global__ void generate_kernel(curandState *state,
                                 unsigned int *result)
 {
-    int id = threadIdx.x + blockIdx.x * 64;
+    int id = threadIdx.x + blockIdx.x * THREADS_PER_BLOCK;
     int count = 0;
     unsigned int x;
     /* Copy state to local memory for efficiency */
@@ -53,247 +48,51 @@ __global__ void generate_kernel(curandState *state,
     result[id] += count;
 }
 
-__global__ void generate_uniform_kernel(curandState *state,
-                                unsigned int *result)
-{
-    int id = threadIdx.x + blockIdx.x * 64;
-    unsigned int count = 0;
-    float x;
-    /* Copy state to local memory for efficiency */
-    curandState localState = state[id];
-    /* Generate pseudo-random uniforms */
-    for(int n = 0; n < 10000; n++) {
-        x = curand_uniform(&localState);
-        /* Check if > .5 */
-        if(x > .5) {
-            count++;
-        }
-    }
-    /* Copy state back to global memory */
-    state[id] = localState;
-    /* Store results */
-    result[id] += count;
-}
-
-__global__ void generate_normal_kernel(curandState *state,
-                                unsigned int *result)
-{
-    int id = threadIdx.x + blockIdx.x * 64;
-    unsigned int count = 0;
-    float2 x;
-    /* Copy state to local memory for efficiency */
-    curandState localState = state[id];
-    /* Generate pseudo-random normals */
-    for(int n = 0; n < 5000; n++) {
-        x = curand_normal2(&localState);
-        /* Check if within one standard deviaton */
-        if((x.x > -1.0) && (x.x < 1.0)) {
-            count++;
-        }
-        if((x.y > -1.0) && (x.y < 1.0)) {
-            count++;
-        }
-    }
-    /* Copy state back to global memory */
-    state[id] = localState;
-    /* Store results */
-    result[id] += count;
-}
-
-__global__ void generate_kernel(curandStateMRG32k3a *state,
-                                unsigned int *result)
-{
-    int id = threadIdx.x + blockIdx.x * 64;
-    unsigned int count = 0;
-    unsigned int x;
-    /* Copy state to local memory for efficiency */
-    curandStateMRG32k3a localState = state[id];
-    /* Generate pseudo-random unsigned ints */
-    for(int n = 0; n < 10000; n++) {
-        x = curand(&localState);
-        /* Check if low bit set */
-        if(x & 1) {
-            count++;
-        }
-    }
-    /* Copy state back to global memory */
-    state[id] = localState;
-    /* Store results */
-    result[id] += count;
-}
-
-__global__ void generate_uniform_kernel(curandStateMRG32k3a *state,
-                                unsigned int *result)
-{
-    int id = threadIdx.x + blockIdx.x * 64;
-    unsigned int count = 0;
-    double x;
-    /* Copy state to local memory for efficiency */
-    curandStateMRG32k3a localState = state[id];
-    /* Generate pseudo-random uniforms */
-    for(int n = 0; n < 10000; n++) {
-        x = curand_uniform_double(&localState);
-        /* Check if > .5 */
-        if(x > .5) {
-            count++;
-        }
-    }
-    /* Copy state back to global memory */
-    state[id] = localState;
-    /* Store results */
-    result[id] += count;
-}
-
-__global__ void generate_normal_kernel(curandStateMRG32k3a *state,
-                                unsigned int *result)
-{
-    int id = threadIdx.x + blockIdx.x * 64;
-    unsigned int count = 0;
-    double2 x;
-    /* Copy state to local memory for efficiency */
-    curandStateMRG32k3a localState = state[id];
-    /* Generate pseudo-random normals */
-    for(int n = 0; n < 5000; n++) {
-        x = curand_normal2_double(&localState);
-        /* Check if within one standard deviaton */
-        if((x.x > -1.0) && (x.x < 1.0)) {
-            count++;
-        }
-        if((x.y > -1.0) && (x.y < 1.0)) {
-            count++;
-        }
-    }
-    /* Copy state back to global memory */
-    state[id] = localState;
-    /* Store results */
-    result[id] += count;
-}
-
 int main(int argc, char *argv[])
 {
     int i;
     unsigned int total;
     curandState *devStates;
-    curandStateMRG32k3a *devMRGStates;
     unsigned int *devResults, *hostResults;
-    bool useMRG = 0;
-    bool doubleSupported = 0;
-    int device;
-    struct cudaDeviceProp properties;
-
-    /* check for double precision support */
-    CUDA_CALL(cudaGetDevice(&device));
-    CUDA_CALL(cudaGetDeviceProperties(&properties,device));
-    if ( properties.major >= 2 || (properties.major == 1 && properties.minor >= 3) ) {
-        doubleSupported = 1;
-    }
-
-    /* Check for MRG32k3a option (default is XORWOW) */
-    if ((argc == 2) && (strcmp(argv[1],"-m") == 0)) {
-        useMRG = 1;
-        if (!doubleSupported){
-            printf("MRG32k3a requires double precision\n");
-            printf("^^^^ test WAIVED due to lack of double precision\n");
-            return EXIT_SUCCESS;
-        }
-    }
 
     /* Allocate space for results on host */
-    hostResults = (unsigned int *)calloc(64 * 64, sizeof(int));
+    hostResults = (unsigned int *)calloc(THREADS_PER_BLOCK * BLOCKS, sizeof(unsigned int));
 
     /* Allocate space for results on device */
-    CUDA_CALL(cudaMalloc((void **)&devResults, 64 * 64 *
+    CUDA_CALL(cudaMalloc((void **)&devResults, THREADS_PER_BLOCK * BLOCKS *
               sizeof(unsigned int)));
 
     /* Set results to 0 */
-    CUDA_CALL(cudaMemset(devResults, 0, 64 * 64 *
+    CUDA_CALL(cudaMemset(devResults, 0, THREADS_PER_BLOCK * BLOCKS *
               sizeof(unsigned int)));
 
     /* Allocate space for prng states on device */
-    if (!useMRG) {
-        CUDA_CALL(cudaMalloc((void **)&devStates, 64 * 64 *
-                  sizeof(curandState)));
-    } else {
-        CUDA_CALL(cudaMalloc((void **)&devMRGStates, 64 * 64 *
-                  sizeof(curandStateMRG32k3a)));
-    }
+    CUDA_CALL(cudaMalloc((void **)&devStates, THREADS_PER_BLOCK * BLOCKS *
+              sizeof(curandState)));
 
-    /* Setup prng states */
-    if (!useMRG) {
-        setup_kernel<<<64, 64>>>(devStates);
-    } else {
-        setup_kernel<<<64, 64>>>(devMRGStates);
-    }
+
+    setup_kernel<<<BLOCKS, THREADS_PER_BLOCK>>>(devStates);
+
 
     /* Generate and use pseudo-random  */
     for(i = 0; i < 50; i++) {
-        if (!useMRG) {
-            generate_kernel<<<64, 64>>>(devStates, devResults);
-        } else {
-            generate_kernel<<<64, 64>>>(devMRGStates, devResults);
-        }
+        generate_kernel<<<BLOCKS, THREADS_PER_BLOCK>>>(devStates, devResults);
     }
 
+
     /* Copy device memory to host */
-    CUDA_CALL(cudaMemcpy(hostResults, devResults, 64 * 64 *
+    CUDA_CALL(cudaMemcpy(hostResults, devResults, THREADS_PER_BLOCK * BLOCKS *
         sizeof(unsigned int), cudaMemcpyDeviceToHost));
+
 
     /* Show result */
     total = 0;
-    for(i = 0; i < 64 * 64; i++) {
+    for(i = 0; i < THREADS_PER_BLOCK * BLOCKS; i++) {
         total += hostResults[i];
     }
     printf("Fraction with low bit set was %10.13f\n",
-        (float)total / (64.0f * 64.0f * 10000.0f * 50.0f));
+        (float)total / (THREADS_PER_BLOCK * BLOCKS * 10000.0f * 50.0f));
 
-    /* Set results to 0 */
-    CUDA_CALL(cudaMemset(devResults, 0, 64 * 64 *
-              sizeof(unsigned int)));
-
-    /* Generate and use uniform pseudo-random  */
-    for(i = 0; i < 50; i++) {
-        if (!useMRG) {
-            generate_uniform_kernel<<<64, 64>>>(devStates, devResults);
-        } else {
-            generate_uniform_kernel<<<64, 64>>>(devMRGStates, devResults);
-        }
-    }
-
-    /* Copy device memory to host */
-    CUDA_CALL(cudaMemcpy(hostResults, devResults, 64 * 64 *
-        sizeof(unsigned int), cudaMemcpyDeviceToHost));
-
-    /* Show result */
-    total = 0;
-    for(i = 0; i < 64 * 64; i++) {
-        total += hostResults[i];
-    }
-    printf("Fraction of uniforms > 0.5 was %10.13f\n",
-        (float)total / (64.0f * 64.0f * 10000.0f * 50.0f));
-    /* Set results to 0 */
-    CUDA_CALL(cudaMemset(devResults, 0, 64 * 64 *
-              sizeof(unsigned int)));
-
-    /* Generate and use uniform pseudo-random  */
-    for(i = 0; i < 50; i++) {
-        if (!useMRG) {
-            generate_normal_kernel<<<64, 64>>>(devStates, devResults);
-        } else {
-            generate_normal_kernel<<<64, 64>>>(devMRGStates, devResults);
-        }
-    }
-
-    /* Copy device memory to host */
-    CUDA_CALL(cudaMemcpy(hostResults, devResults, 64 * 64 *
-        sizeof(unsigned int), cudaMemcpyDeviceToHost));
-
-    /* Show result */
-    total = 0;
-    for(i = 0; i < 64 * 64; i++) {
-        total += hostResults[i];
-    }
-    printf("Fraction of normals within 1 standard deviation was %10.13f\n",
-        (float)total / (64.0f * 64.0f * 10000.0f * 50.0f));
 
     /* Cleanup */
     if (!useMRG) {
