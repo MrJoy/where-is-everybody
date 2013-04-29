0a2ea17c0ba6772fc987568842365a1e1f040983
Jon Frisby <jfrisby@mrjoy.com>
2013-04-29 00:38:13 -0700

    Changing hello.cu to test out Hemi.
    Conflicts:
    	simulator/Makefile

--- simulator/src/hello.cu
+++ simulator/src/hello.cu
@@ -1,79 +1,90 @@
-#include <stdio.h>
-#include "curand_kernel.h"
+#include <stdint.h>
+#include <cstdio>
+#include <algorithm>
 
-const int N = 16;
-const int blocksize = 16;
+#include "hemi/hemi.h"
+#include "hemi/array.h"
 
-__global__
-void
-hello(unsigned int *seeds, unsigned int *outs, int n)
+#include "wie/device.h"
+#include "wie/stopwatch.h"
+
+#define N 64*1024*1024
+
+HEMI_DEV_CALLABLE_INLINE
+void initElement(uint32_t* data, int idx)
 {
-  curandStateXORWOW_t rgen;
-  int baseIdx = threadIdx.x + blockIdx.x * blockDim.x;
-  curand_init( seeds[baseIdx], 0, 0, &rgen );
-  for( int i=0; i<n; ++i ) {
-    outs[threadIdx.x * n + i] = curand( &rgen );
+  // printf( "%d ", idx );
+  if ( idx < N ) {
+    data[idx] = idx;
+  } else {
+    printf( "WTF?! %d\n", idx );
   }
-  // printf("threadIdx.x=%d, blockIdx.x=%d, blockDim.x=%d, seed=%08x\n", threadIdx.x, blockIdx.x, blockDim.x, seeds[threadIdx.x]);
-  //see float curand_uniform
-  //see curand__discrete
 }
 
-void
-get_seeds( unsigned int *seeds, int n )
+HEMI_KERNEL(init)(uint32_t* data)
 {
-  FILE* randomSource = fopen("/dev/random", "rb");
-  unsigned int seed0;
-  int recordsRead = fread( &seed0, sizeof(unsigned int), 1, randomSource );
-  //assert(recordsRead == n);
-  for( int i=0; i<n; ++i ) {
-    seeds[i] = seed0 + i;
+  int offset = hemiGetElementOffset();
+  int stride = hemiGetElementStride();
+
+  for( int idx = offset; idx < N; idx += stride ) {
+    initElement( data, idx );
   }
-  fclose( randomSource );
 }
 
-void
-inspect( unsigned int *array, int n )
+HEMI_DEV_CALLABLE_INLINE
+void benchmarkElement(const uint32_t* inputs, uint32_t* outputs, int idx)
 {
-  for( int i=0; i<n; ++i ) {
-    printf( "%08x ", array[i] );
+  // printf( "%d ", idx );
+  if( idx < N ) {
+    outputs[idx] = inputs[idx];
+  } else {
+    printf("WTF?! %d\n", idx);
   }
-  printf( "\n" );
+}
+
+HEMI_KERNEL(benchmark)(const uint32_t* inputs, uint32_t* outputs)
+{
+  int offset = hemiGetElementOffset();
+  int stride = hemiGetElementStride();
+
+  for( int idx = offset; idx < N; idx += stride ) {
+    benchmarkElement( inputs, outputs, idx );
+  }
 }
 
 int
 main()
 {
-  unsigned int seeds[N];
-  unsigned int outputs[N * N];
-  for( int j=0; j<N*N; j++ ) {
-    outputs[j] = 0;
+  using std::min;
+  using std::cout;
+  using std::endl;
+  using std::setprecision;
+
+  const int blockDim = 128;
+  const int gridDim = min<int>(1024, (N + blockDim - 1) / blockDim);
+  cout << "blockDim=" << blockDim << ", gridDim=" << gridDim << endl;
+
+  WIE::Device device(0);
+  WIE::Stopwatch stopwatch(device);
+
+  cout << "Preparing input buffer..." << endl;
+  hemi::Array<uint32_t> inputs(N, false);
+  cout << "Preparing output buffer..." << endl;
+  hemi::Array<uint32_t> outputs(N, false);
+
+  cout << "Initializing input buffer..." << endl;
+  HEMI_KERNEL_LAUNCH(init, gridDim, blockDim, 0, 0, inputs.writeOnlyPtr());
+
+  cout << "Benchmarking..." << endl;
+  for(int i = 0; i < 10; i++) {
+    cout << "Iteration: " << i << endl;
+    stopwatch.start();
+    HEMI_KERNEL_LAUNCH(benchmark, gridDim, blockDim, 0, 0, inputs.readOnlyPtr(), outputs.writeOnlyPtr());
+    stopwatch.stop();
+    stopwatch.reportLast(cout);
   }
+  cout << endl;
+  stopwatch.reportAverage(cout);
 
-  unsigned int *cseeds;
-  unsigned int *coutputs;
-  const int seed_size = N*sizeof( unsigned int );
-  const int output_size = N*N*sizeof( unsigned int );
-
-  get_seeds( &seeds[0], N );
-  inspect( seeds, N );
-  printf( "\n" );
-
-  cudaMalloc( (void**)&cseeds, seed_size );
-  cudaMalloc( (void**)&coutputs, output_size );
-  cudaMemcpy( cseeds, seeds, seed_size, cudaMemcpyHostToDevice );
-  cudaMemcpy( coutputs, outputs, output_size, cudaMemcpyHostToDevice );
-
-  dim3 dimBlock( blocksize, 1 );
-  dim3 dimGrid( 1, 1 );
-  //TODO, don't pass N, use dim
-  hello<<<dimGrid, dimBlock>>>( cseeds, coutputs, N );
-  cudaMemcpy( outputs, coutputs, output_size, cudaMemcpyDeviceToHost );
-  cudaFree( cseeds );
-  cudaFree( coutputs );
-
-  for( int i=0; i<N; ++i ) {
-    inspect( outputs+i*N, N );
-  }
   return EXIT_SUCCESS;
 }
