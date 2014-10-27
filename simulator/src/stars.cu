3cb98aba8ef0e758a21d1613acb32952e57d80c7
Matt Grosso <mgrosso@acm.org>
2014-10-26 19:22:36 -0700

    refactor, add SPACE_FARING, s/INIT/PROTOSTAR/
    also change statematrix to be indexed by 2 * old_state
    so the unflipped and flipped states are adjacent

--- simulator/src/stars.cu
+++ simulator/src/stars.cu
@@ -27,7 +27,7 @@ const char * OUTPUT_T_FORMAT = "%u" ;
 /*
 // the code I want
 enum class States : output_t { 
-  INIT = 0,
+  PROTOSTAR = 0,
   UNINHABITABLE = 1,
   INHABITABLE = 2,
   CELLULAR = 3, 
@@ -39,16 +39,17 @@ enum class States : output_t {
 */
 // the code I have: nvcc does not support -std=c++0x.
 // I am refusing to deal with pre c++11 enum.
-const output_t  INIT = 0;
+const output_t  PROTOSTAR = 0;
 const output_t  UNINHABITABLE = 1;
 const output_t  INHABITABLE = 2;
 const output_t  CELLULAR = 3;
 const output_t  OXYGEN_EVENT = 4;
 const output_t  CAMBRIAN_EVENT = 5;
 const output_t  TECHNICAL_CIV = 6;
+const output_t  SPACE_FARING = 7;
 
-const unsigned int  NUM_STATES = 7;
- 
+const unsigned int  NUM_STATES = 8;
+
 // **********************************************************************
 // each step is a boolean trial representing 10myr.
 //
@@ -63,43 +64,45 @@ const unsigned int  NUM_STATES = 7;
 // note civ states (eg: spacefaring, collapse, colonize) are modeled 
 // separately in fast time since 10myr steps don't seem reasonable
 //
-// STATE_MATRIX is indexed by pseudo code
+// STATE_CHANGES is indexed by pseudo code
 //   flip{0,1} * NUM_STATES + current_state{0..(NUM_STATES -1)}
 //
 //   where flip=0 implies transistion and 1 implies stasis and
 //   where the value returned by that is the next state
 //**********************************************************************
 
-const output_t STATE_MATRIX [14] =
-{
-  //transition
-  INHABITABLE,
-  UNINHABITABLE,
-  CELLULAR,
-  OXYGEN_EVENT,
-  CAMBRIAN_EVENT,
-  TECHNICAL_CIV,
-  TECHNICAL_CIV,
-  //stasis
-  UNINHABITABLE,
-  UNINHABITABLE,
-  INHABITABLE,
-  CELLULAR,
-  OXYGEN_EVENT,
-  CAMBRIAN_EVENT,
-  TECHNICAL_CIV
-};
-
-
-
+const float pZ = 0.0 ;   //uninhabitable
 const float pH = 0.5 ;   //habitable
 const float pB = 0.007 ; //Cellular Biology (50% chance after 100*10myr )
 const float pO = 0.007 ; //Oxygen Event (took another 100 iterations on earth)
-const float pM = 0.0035; //Cambrian Explosion (took 200 iterations on earth)
-const float pT = 0.014;  //Technical Civilization
-//const float pS = 0.5  ;  //Space Faring Civilization
+const float pM = 0.0035; //Cambrian Explosion of [M]ulticellular life (took 200 iterations on earth)
+const float pT = 0.014;  //Technical Civilization (radio telescope)
+const float pS = 0.5  ;  //Space Faring Civilization (or collapse) per 1000 years of Tech Civ
+const float pC = 0.5  ;  //spacefaring local solar system civ collapse
 
-const float PCHANGE[7] = { pH, 0.0, pB, pO, pM, pT, 0.0 };
+const output_t STATE_CHANGES [ NUM_STATES * 2 ] =
+{
+  UNINHABITABLE,  INHABITABLE,      //0,1 states from PROTOSTAR,
+  UNINHABITABLE,  UNINHABITABLE,    //0,1 states from UNINHABITABLE
+  INHABITABLE,    CELLULAR,         //0,1 states from INHABITABLE
+  CELLULAR,       OXYGEN_EVENT,     //0,1 states from CELLULAR
+  OXYGEN_EVENT,   CAMBRIAN_EVENT,   //0,1 states from OXYGEN_EVENT
+  CAMBRIAN_EVENT, TECHNICAL_CIV,    //0,1 states from CAMBRIAN_EVENT
+  CAMBRIAN_EVENT, SPACE_FARING,     //0,1 states from TECHNICAL_CIV
+  SPACE_FARING,   TECHNICAL_CIV,    //0,1 states from SPACE_FARING
+};
+
+const float P_CHANGE [ NUM_STATES ] =
+{
+  pH,  //p(change) from PROTOSTAR,
+  pZ,  //p(change) from UNINHABITABLE
+  pB,  //p(change) from INHABITABLE
+  pO,  //p(change) from CELLULAR
+  pM,  //p(change) from OXYGEN_EVENT
+  pT,  //p(change) from CAMBRIAN_EVENT
+  pS,  //p(change) from TECHNICAL_CIV
+  pC,  //p(change) from SPACE_FARING
+};
 
 
 __global__ void
@@ -115,28 +118,59 @@ init_buf( output_t *outs, int n)
 {
   int baseIdx = threadIdx.x + blockIdx.x * blockDim.x;
   for( int i=0; i<n; ++i ) {
-    outs[baseIdx * n + i] = INIT;
+    outs[baseIdx * n + i] = PROTOSTAR;
   }
 }
 
 __global__ void
-iterate_states( curandStateXORWOW_t *rgens, output_t *buf_in, output_t *buf_out, int neighborhood_stars, output_t *state_matrix, float *pchange )
+iterate_states(
+    curandStateXORWOW_t *rgens,
+    output_t *buf_in,
+    output_t *buf_out,
+    int neighborhood_stars,
+    output_t *state_matrix,
+    float *pchange//,
+    //output_t *buf_old_states,
+    //float *buf_rnds,
+    //unsigned int *_flips
+    )
 {
   int neighborhood = threadIdx.x + blockIdx.x * blockDim.x;
   int base = neighborhood * neighborhood_stars;
   curandStateXORWOW_t rgen = rgens[neighborhood];
   for( int i=0; i<neighborhood_stars; ++i ) {
-    int star = base + i;
+    const int star = base + i;
     output_t old_state = buf_in[star];
-    unsigned int flip = (unsigned int) ceil(
-        curand_uniform(&rgen) - pchange[ old_state ]);
-    buf_out[star] = state_matrix[ flip * NUM_STATES + old_state ];
 
-    //int matrix_idx = flip * NUM_STATES + old_state ;
-    //printf("neighborhood=%i neighborhood_stars=%i i=%i star=%i old_state=%i new_state=%i flip=%i p=%f matrix_idx=%i\neighborhood_stars",
-    //  neighborhood, neighborhood_stars, i, 
-    //  star, old_state, buf_out[star], flip,
-    //  pchange[ old_state], matrix_idx );
+    //float rnd = curand_uniform(&rgen);
+    //float p = pchange[ old_state ];
+    //unsigned int flip = 1 - (unsigned int) ceil( rnd - p );
+    //unsigned int new_state_index = old_state * 2 + flip ;
+    //output_t new_state = state_matrix[ new_state_index ];
+    //buf_out[star] = new_state;
+    //buf_out[star] = i % NUM_STATES;
+
+    //printf("neighborhood=%i neighborhood_stars=%i i=%i star=%i old_state=%i rnd=%f p=%f flip=%i new_state_index=%i new_state=%i\n",
+    //  neighborhood,
+    //  neighborhood_stars,
+    //  i,
+    //  star,
+    //  old_state,
+    //  rnd,
+    //  p,
+    //  flip,
+    //  new_state_index,
+    //  new_state
+    //  );
+    //
+    // the the inner loop was before I hacked in the debug version above
+    //
+    buf_out[star] = state_matrix[
+        old_state * 2 +
+        (1 -
+          (unsigned int) ceil(curand_uniform(&rgen) -
+          pchange[ old_state ]))
+      ];
   }
   rgens[neighborhood] = rgen;
 }
@@ -191,8 +225,11 @@ main()
   cudaMalloc( (void**)&cpchange, pchange_size );
   cudaMalloc( (void**)&crgens, rgen_size );
 
-  cudaMemcpy( cstate_matrix, STATE_MATRIX, state_matrix_size, cudaMemcpyHostToDevice );
-  cudaMemcpy( cpchange, PCHANGE, pchange_size, cudaMemcpyHostToDevice );
+
+  //TODO: are these being copied to the host correctly?
+  cudaMemcpy( cstate_matrix, STATE_CHANGES, state_matrix_size, cudaMemcpyHostToDevice );
+  cudaMemcpy( cpchange, P_CHANGE, pchange_size, cudaMemcpyHostToDevice );
+
 
   init_buf<<<BLOCKS, THREADS_PER_BLOCK>>>( couts1, NEIGHBORHOOD_STARS );
   init_buf<<<BLOCKS, THREADS_PER_BLOCK>>>( couts2, NEIGHBORHOOD_STARS );
